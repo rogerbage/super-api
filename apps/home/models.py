69918e1b0329d1235e0ce5d41134558451ae644b
Roger Borges <correiodoroger@gmail.com>
2024-07-24 15:02:24 -0300

    feat: remove unused database model

--- apps/home/models.py
+++ apps/home/models.py
@@ -2,12 +2,3 @@ from flask_login import UserMixin
 
 from apps import db
 
-class Empresa(db.Model, UserMixin):
-
-    __tablename__ = 'Empresas'
-
-    id            = db.Column(db.Integer, primary_key=True)
-    cnpj          = db.Column(db.String(20), unique=True)
-    nome_razao    = db.Column(db.String(100))
-    nome_fantasia = db.Column(db.String(255))
-    cnae          = db.Column(db.String(8))
